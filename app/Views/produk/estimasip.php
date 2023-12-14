5ad9deff9ee499a656bd88678ed8334d9397255f
andreass7 <adiieee0506@gmail.com>
2023-12-14 11:47:15 +0700

    Update estimasip.php

--- app/Views/produk/estimasip.php
+++ app/Views/produk/estimasip.php
@@ -35,9 +35,36 @@
     </div>
     <div class="d-flex justify-content-center mb-2">
         <button type="submit" class="btn btn-dark">Estimasi</button>
-        <button type="submit" class="btn btn-light">Masuk Sebagai Admin</button>
+        <button type="submit" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#login">Masuk Sebagai Admin</button>
     </div>
 </div>
 <!-- end Form -->
 
+<!-- Modal Login -->
+<div class="modal" tabindex="-1" id="login">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Login Sebagai Admin</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                <label for="inputPassword5" class="form-label">Password</label>
+                <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+                <div id="passwordHelpBlock" class="form-text">
+                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Modal -->
 <?php $this->endSection() ?>
